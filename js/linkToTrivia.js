42b7385f4fbc2bc975e35200e1e77a47894f969b
Chen Gutt <chen.gutt1@gmail.com>
2021-03-17 10:13:17 +0100

    commit -m

--- js/linkToTrivia.js
+++ js/linkToTrivia.js
@@ -7,9 +7,9 @@ function playTrivia(btnElement) {
 
     btnElement.addEventListener('click', () => {
         if (screen.width < 768) {
-            newwindow = window.open("triviaGame.html", 'trivia', 'screen.width,screen.height');
+            window.open("triviaGame.html", 'trivia', 'screen.width,screen.height');
         } else {
-            newwindow = window.open("triviaGame.html", 'trivia', 'height=600,width=350');
+            window.open("triviaGame.html", 'trivia', 'height=600,width=350');
         }
 
 
